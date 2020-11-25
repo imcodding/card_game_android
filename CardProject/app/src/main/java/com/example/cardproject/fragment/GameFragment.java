772c51b927605932295850c0ad23ae02b9f11999
DESKTOP-TV3GRHM\yd043 <suy0226@naver.com>
2020-11-25 17:56:47 +0900

    모듈화 진행 중

--- CardProject/app/src/main/java/com/example/cardproject/fragment/GameFragment.java
+++ CardProject/app/src/main/java/com/example/cardproject/fragment/GameFragment.java
@@ -54,6 +54,7 @@ public class GameFragment extends Fragment implements OnItemClickListener {
     int TOTAL, ANSWER, RANDOM_SIZE; // RANDOM_SIZE: 뒤집어지는 쌍 개수
 
 //    Thread thread;
+    private Game mGame;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,56 +62,53 @@ public class GameFragment extends Fragment implements OnItemClickListener {
 
         View view = inflater.inflate(R.layout.fragment_game, container, false);
 
+        GridLayoutManager manager = new GridLayoutManager(getContext(), 3);
+        mGameRvCardList = view.findViewById(R.id.game_rv_card_list);
+        mGameRvCardList.setLayoutManager(manager);
         mProgressBar = view.findViewById(R.id.progressBar);
         mGameTvTime =  view.findViewById(R.id.game_tv_time);
         mGameTvScore = view.findViewById(R.id.game_tv_score);
-        mGameRvCardList = view.findViewById(R.id.game_rv_card_list);
 
         valueSet();
 
-        cardAndImageSetting();
-
-        shuffleCards();
-
-        GridLayoutManager manager = new GridLayoutManager(getContext(), 3);
-        mGameRvCardList.setLayoutManager(manager);
-        mCardAdapter = new CardAdapter(cards);
+        mGame = new Game();
+        mGame.ready();
+//        cardAndImageSetting();
+//        shuffleCards();
+//        mCardAdapter = new CardAdapter(cards);
+        mCardAdapter = new CardAdapter(mGame.getCards());
         mCardAdapter.setListener(this);
         mGameRvCardList.setAdapter(mCardAdapter);
 
-        startTimer();
-
-        resetThread(2000);
-
+        gameStart();
 
         return view;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        stopTimer();
+    private void gameStart() {
+        startTimer();
+        resetThread(2000);
     }
 
     @Override
     public void onItemClick(View view, int pos) {
 
-        Card card = cards.get(pos);
+        Card card = mGame.getCardItem(pos);
         ImageView ivCard = view.findViewById(R.id.iv_card);
         ObjectAnimator animator = ObjectAnimator.ofFloat(ivCard, "rotationY", 0f, 180f);
 
-        if(selected1 == 0) {
-            selected1 = card.getDefaultImgId();
-        } else {
-            selected2 = card.getDefaultImgId();
-        }
+//        if(selected1 == 0) {
+//            selected1 = card.getDefaultImgId();
+//        } else {
+//            selected2 = card.getDefaultImgId();
+//        }
 
         animator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
                 ivCard.setEnabled(false);
-                ivCard.setImageResource(card.getDefaultImgId());
+//                ivCard.setImageResource(card.getDefaultImgId());
                 count += 1;
                 totalCount += 1;
 
@@ -171,74 +169,6 @@ public class GameFragment extends Fragment implements OnItemClickListener {
             shuffleCards();
         }
     }
-
-    private void cardAndImageSetting() {
-        images = new ArrayList<>();
-        images.add(R.drawable.image1);
-//        images.add(R.drawable.image2);
-        images.add(R.drawable.image3);
-        images.add(R.drawable.image4);
-        images.add(R.drawable.image5);
-        images.add(R.drawable.image6);
-        images.add(R.drawable.image7);
-
-        cards = new ArrayList<>();
-        for(int i = 0; i < 12; i++) {
-            cards.add(new Card());
-        }
-    }
-
-    private void shuffleCards() {
-        // 이미지 뽑기
-        ArrayList<Integer> randomImages = new ArrayList<>();
-        for(int i = 0; i < RANDOM_SIZE; i++) {
-            int ranNum = (int)(Math.random() * 6); // 이미지 개수
-            if(randomImages.contains(ranNum)) {
-                i--;
-            } else {
-                randomImages.add(ranNum);
-            }
-        }
-        randomImages.addAll(randomImages);
-
-        // 실제 12개 중에 이미지가 변경될 index
-        ArrayList<Integer> setImageIndexes = new ArrayList<>();
-        for(int i = 0; i < RANDOM_SIZE * 2; i++) {
-            int ranNum = (int)(Math.random() * 12);
-            if(setImageIndexes.contains(ranNum)) {
-                i--;
-            } else {
-                setImageIndexes.add(ranNum);
-            }
-        }
-
-        for(int i = 0; i < setImageIndexes.size(); i++) {
-            cards.get(setImageIndexes.get(i)).setDefaultImgId(images.get(randomImages.get(i)));
-        }
-    }
-
-    private void resetThread(int milli) {
-        Handler handler = new Handler();
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(milli);
-                    mCardAdapter.reset();
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mCardAdapter.notifyDataSetChanged();
-                        }
-                    });
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        thread.start();
-    }
-
 
     private void startTimer() {
         mTimerTask = new TimerTask() {
@@ -347,4 +277,80 @@ public class GameFragment extends Fragment implements OnItemClickListener {
         selected1 = 0;
         selected2 = 0;
     }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        stopTimer();
+    }
+
+    private void cardAndImageSetting() {
+        images = new ArrayList<>();
+        images.add(R.drawable.image1);
+//        images.add(R.drawable.image2);
+        images.add(R.drawable.image3);
+        images.add(R.drawable.image4);
+        images.add(R.drawable.image5);
+        images.add(R.drawable.image6);
+        images.add(R.drawable.image7);
+
+        cards = new ArrayList<>();
+        for(int i = 0; i < 12; i++) {
+            cards.add(new Card());
+        }
+    }
+
+    private void shuffleCards() {
+        // 이미지 뽑기
+        ArrayList<Integer> randomImages = new ArrayList<>();
+        for(int i = 0; i < RANDOM_SIZE; i++) {
+            int ranNum = (int)(Math.random() * 6); // 이미지 개수
+            if(randomImages.contains(ranNum)) {
+                i--;
+            } else {
+                randomImages.add(ranNum);
+            }
+        }
+        randomImages.addAll(randomImages);
+
+        // 실제 12개 중에 이미지가 변경될 index
+        ArrayList<Integer> setImageIndexes = new ArrayList<>();
+        for(int i = 0; i < RANDOM_SIZE * 2; i++) {
+            int ranNum = (int)(Math.random() * 12);
+            if(setImageIndexes.contains(ranNum)) {
+                i--;
+            } else {
+                setImageIndexes.add(ranNum);
+            }
+        }
+
+        for(int i = 0; i < setImageIndexes.size(); i++) {
+            cards.get(setImageIndexes.get(i)).setDefaultImgId(images.get(randomImages.get(i)));
+        }
+    }
+
+    private void resetThread(int milli) {
+        Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(milli);
+                    mCardAdapter.reset();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mCardAdapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+
 }
--- CardProject/app/src/main/java/com/example/cardproject/model/Game2.java
+++ CardProject/app/src/main/java/com/example/cardproject/model/Game2.java
@@ -4,34 +4,40 @@ import com.example.cardproject.dummy.DummyData;
 
 import java.util.ArrayList;
 
-public class Game {
-    private int cardCount;
-    private int randomCoupleSize;
-    private int answerCount;
-    private int gameTime;
-    private ArrayList<Integer> mImageDrawables; // 카드 이미지
-    private ArrayList<Card> mCards;
+public class Game2 {
+    public static int CARD_COUNT = 12;
+    public static int GAME_TIME = 60; // 게임시간(초단위)
+    public static int SELECT_CNT = 2;
+    public static int RANDOM_COUPLE_SIZE = 2; // 뒤집어지는 카드 쌍 개수
+    public static int ANSWER_CNT = RANDOM_COUPLE_SIZE;
+    public static int TOTAL_SELECT_CNT;
 
-    public Game() {
-        cardCount = 12;
-        randomCoupleSize = 2;
-        answerCount = 2;
+    private static ArrayList<Integer> mImageDrawables; // 카드 이미지
+    private static ArrayList<Card> mCards;
 
+    public static void start() {
+        initVariable();
+        resetValue();
+        shuffleCards();
+    }
+
+    private static void initVariable() {
         mImageDrawables = new DummyData().getImageDrawables();
         mCards = new ArrayList<>();
-        for(int i = 0; i < cardCount; i++) {
+        for(int i = 0; i < CARD_COUNT; i++) {
             mCards.add(new Card());
         }
     }
 
-    public void ready() {
-        shuffleCards();
+    private static void resetValue() {
+        ANSWER_CNT = RANDOM_COUPLE_SIZE;
+        TOTAL_SELECT_CNT = ANSWER_CNT * 2;
     }
 
-    private void shuffleCards() {
+    private static void shuffleCards() {
         // 이미지 뽑기
         ArrayList<Integer> randomImages = new ArrayList<>();
-        for(int i = 0; i < randomCoupleSize; i++) {
+        for(int i = 0; i < RANDOM_COUPLE_SIZE; i++) {
             int ranNum = (int)(Math.random() * mImageDrawables.size()); // 이미지 개수
             if(randomImages.contains(ranNum)) {
                 i--;
@@ -44,8 +50,8 @@ public class Game {
         // 실제 12개 중에 이미지가 변경될 index
         // 12개의 카드 중에 이미지를 어디에다가 넣을건지.
         ArrayList<Integer> setImageIndexes = new ArrayList<>();
-        for(int i = 0; i < randomCoupleSize * 2; i++) {
-            int ranNum = (int)(Math.random() * cardCount);
+        for(int i = 0; i < RANDOM_COUPLE_SIZE * 2; i++) {
+            int ranNum = (int)(Math.random() * CARD_COUNT);
             if(setImageIndexes.contains(ranNum)) {
                 i--;
             } else {
@@ -61,28 +67,18 @@ public class Game {
         }
     }
 
-    private void changeLevel(int score) {
+    private static void changeLevel(int score) {
         if(score < 2000) {
             shuffleCards();
         } else if(score < 4000) {
-            randomCoupleSize = 3;
-            answerCount = randomCoupleSize;
+            RANDOM_COUPLE_SIZE = 3;
+            ANSWER_CNT = RANDOM_COUPLE_SIZE;
             shuffleCards();
         } else {
-            randomCoupleSize = 4;
+            RANDOM_COUPLE_SIZE = 4;
             shuffleCards();
         }
     }
 
-    public ArrayList<Card> getCards() {
-        return mCards;
-    }
 
-    public int getGameTime() {
-        return gameTime;
-    }
-
-    public Card getCardItem(int pos) {
-        return mCards.get(pos);
-    }
 }
